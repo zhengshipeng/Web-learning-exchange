94cd37cc05de8a00eef6c71658b8fec59e56cc69
zhengshipeng <370019506@qq.com>
2016-03-15 11:40:46 +0800

    修改注释

--- PHP/phpAPP/1.php
+++ PHP/phpAPP/1.php
@@ -2,11 +2,11 @@
 
 $redis = new Redis();
 $redis->connect("127.0.0.1","6379");
-//存储一个 值
+//存储一个值set,get
 $redis->set("say","Redis test!");
 echo $redis->get("say");
 
-//存储多个值
+//存储多个值mset,mget
 $array = array('first_key'=>'first_val',
           'second_key'=>'second_val',
           'third_key'=>'third_val');
